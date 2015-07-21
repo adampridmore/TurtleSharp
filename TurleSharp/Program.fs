9d013bdbdf906b376fed7f39600858692abc5ecb
Adam Pridmore <adam_pridmore@hotmail.com>
2015-07-21 21:08:52 +0100

    - Moved some methods to the Turtle module.  - Small tidy up.

--- TurleSharp/Program.fs
+++ TurleSharp/Program.fs
@@ -3,10 +3,7 @@ open System.Drawing
 open TurtleTypes
 open Turtle
 
-let width = 640.
-let height = 480.
-
-let showImage (c:Canvas) = 
+let showCanvas (c:Canvas) = 
     let f = new System.Windows.Forms.Form();
     f.Width <- c.i.Width
     f.Height <- c.i.Height + 50
@@ -20,42 +17,22 @@ let showImage (c:Canvas) =
     f.ShowDialog() |> ignore
     //f.Show() |> ignore
 
-let toCanvas (p:Position) = new Point(p.x |> int, (height - p.y) |> int )
-
-let drawLine (pos:Position) (vector:Vector) (canvas: Canvas) =
-  let p1 = pos
-  let p2 = pos |> translate vector
-  canvas.g.DrawLine(canvas.p, p1 |> toCanvas , p2 |> toCanvas) |> ignore
-  canvas
-
-let moveTutle (vector:Vector) (canvas: Canvas) =
-  let absoluteDirection = canvas.turtle.direction + vector.direction
-  let p1 = canvas.turtle.position
-  let p2 = p1 |> translate {vector with direction = absoluteDirection}
-  
-  canvas.g.DrawLine(canvas.p, p1 |> toCanvas , p2 |> toCanvas) |> ignore
-  
-  { canvas with turtle = { canvas.turtle with position = p2; direction = absoluteDirection} }
+let blankCanvas = 
+  let image = new Bitmap((canvasWidth |> int), (canvasHeight |> int))
+  let turtle = { position = { x = canvasWidth / 2. ; y = canvasHeight / 2.}; direction = 0. }
+  {g = Graphics.FromImage(image); p = new Pen(new SolidBrush(Color.Black)); i = image; turtle = turtle}
 
 [<EntryPoint>]
 let main argv =
-    let image = new Bitmap((width |> int), (height |> int))
-    let g = Graphics.FromImage(image);
-    let p = new Pen(new SolidBrush(Color.Black))
-
-    let canvas = { 
-        g = g; 
-        p = p;
-        i = image;
-        turtle = { position = {x = width / 2. ; y = height / 2.};direction = 0.} 
-    }
+    let numberOfSides = 5
+    let lengthInc = 50.
+    let directionIncrement = 360. / (numberOfSides |> float)
     
-    Seq.unfold (fun (state:Canvas) -> let newC = state |> moveTutle {length=2.;direction=1. }
-                                      Some(newC, newC)) canvas
-    |> Seq.take 360
+    Seq.unfold (fun canvas -> let newCanvas = canvas |> moveTutle {length=lengthInc;direction=directionIncrement }
+                              Some(newCanvas, newCanvas)) blankCanvas
+    |> Seq.take numberOfSides
     |> Seq.last
-    |> showImage
-
+    |> showCanvas
 //    canvas 
 //    |> moveTutle {length= 100;direction= 45 }
 //    |> moveTutle {length= 100;direction= 45 }
--- TurleSharp/Turtle.fs
+++ TurleSharp/Turtle.fs
@@ -2,6 +2,10 @@
 
 open TurtleTypes
 open System
+open System.Drawing
+
+let canvasWidth = 640.
+let canvasHeight = 480.
 
 let sin x = Math.Sin(x)
 let cos x = Math.Cos(x)
@@ -10,6 +14,8 @@ let PI = Math.PI |> double
 let degreesToRadians (degrees:double) = 
   degrees * PI / 180.
 
+let toCanvas (p:Position) = new Point(p.x |> int, (canvasHeight - p.y) |> int )
+
 let translate (v:Vector) (p:Position) = 
   let hoz = (v.length |> double) * sin (v.direction |> degreesToRadians)
   let vert = (v.length |> double) * cos (v.direction |> degreesToRadians);
@@ -17,4 +23,19 @@ let translate (v:Vector) (p:Position) =
     x= p.x + hoz;
     y= p.y + vert
   }
+
+let drawLine (pos:Position) (vector:Vector) (canvas: Canvas) =
+  let p1 = pos
+  let p2 = pos |> translate vector
+  canvas.g.DrawLine(canvas.p, p1 |> toCanvas , p2 |> toCanvas) |> ignore
+  canvas
+
+let moveTutle (vector:Vector) (canvas: Canvas) =
+  let absoluteDirection = canvas.turtle.direction + vector.direction
+  let p1 = canvas.turtle.position
+  let p2 = p1 |> translate {vector with direction = absoluteDirection}
+  
+  canvas.g.DrawLine(canvas.p, p1 |> toCanvas , p2 |> toCanvas) |> ignore
+  
+  { canvas with turtle = { canvas.turtle with position = p2; direction = absoluteDirection} }
 
